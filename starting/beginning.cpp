7a99a0d66e0927161955374d2c8fc2265301979c
Parna324 <parnaghosh628@gmail.com>
2024-06-01 22:09:54 +0530

    C++

---
+++ starting/beginning.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout <<"Hey Suraj!" <<"\n" << "Hey Parna!";
+    return 0;
+    
+}
---
+++ starting/datatypes.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    //int
+    int x=10;
+    //long
+    long y=15;
+    //long long 
+    long long z = 30;
+    //float double
+    float c=2.33333;
+    double b = 4.7897923923;
+    //string or getline
+    string s1= "hey love";
+    string s2 ="hey honey";
+    cin >> s1,s2;
+    cout << s1<< " " <<s2;
+    //getline:its put everything in the entire line 
+    string str;
+    getline(cin,str);
+    cout << str;
+
+    
+    return 0;
+    
+}
