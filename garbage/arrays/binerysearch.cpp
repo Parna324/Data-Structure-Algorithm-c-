2af01a41e4db6f0d43866f1da3914297dc3935f7
Parna324 <parnaghosh628@gmail.com>
2024-07-19 11:07:46 +0530

    binerysearch

--- garbage/arrays/binerysearch.cpp
+++ garbage/arrays/binerysearch.cpp
@@ -1,3 +1,32 @@
 //iterative binery search
 #include<bits/stdc++.h>
 using namespace std;
+int binarysearch(int arr[], int low, int high, int x){
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(arr[mid]==x)
+        return mid;
+
+        if(arr[mid]<x)
+        low = mid+1;
+
+        else
+        high = mid - 1;
+
+
+        
+    }
+    return -1;
+
+}
+int main(void){
+    int arr[] = {7,8,4,9,2};
+    int x = 4;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarysearch(arr, 0, n - 1, x);
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+    return 0;
+
+}
